e9d0833fbcf4f3f241dbaab798b6254ea1ce10a0
Jesse Stevenson <jessedusty@gmail.com>
2015-11-13 15:06:32 -0500

    added ideas generated in class

---
+++ ideas.mm
@@ -0,0 +1,73 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="ideas" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1447442006790"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="OmniBike" POSITION="right" ID="ID_330591145" CREATED="1447442007027" MODIFIED="1447442926283">
+<edge COLOR="#ff0000"/>
+<node TEXT="w/ derailer" ID="ID_1561738484" CREATED="1447442040879" MODIFIED="1447442047477"/>
+<node TEXT="free-wheel mechanism" ID="ID_746438845" CREATED="1447442051209" MODIFIED="1447442063228"/>
+<node TEXT="(possibly folding)" ID="ID_1182452349" CREATED="1447442063420" MODIFIED="1447442072958"/>
+<node TEXT="light / corosion resistant materials" ID="ID_1236716828" CREATED="1447442836467" MODIFIED="1447442845237">
+<node TEXT="aluminum" ID="ID_827966593" CREATED="1447442845457" MODIFIED="1447442848295"/>
+<node TEXT="carbon fiber" ID="ID_123648397" CREATED="1447442848500" MODIFIED="1447442857654"/>
+</node>
+<node TEXT="generator and electronics" ID="ID_24416105" CREATED="1447442491538" MODIFIED="1447442507238">
+<node TEXT="anti-theft mechaism (possibly electronic)" ID="ID_1510983863" CREATED="1447442079694" MODIFIED="1447442179509"/>
+<node TEXT="built in spedometer" ID="ID_1052865260" CREATED="1447442181119" MODIFIED="1447442197537"/>
+<node TEXT="built in lights and bell" ID="ID_1807272610" CREATED="1447442198669" MODIFIED="1447442362433"/>
+<node TEXT="iphone charger" ID="ID_558305280" CREATED="1447442362948" MODIFIED="1447442456803"/>
+</node>
+</node>
+<node TEXT="Lenovo X200 Tablet" POSITION="right" ID="ID_1303622549" CREATED="1447443038633" MODIFIED="1447443044567">
+<edge COLOR="#0000ff"/>
+</node>
+</node>
+</map>
